9efa97d5c1a573f8199f331df07c15997ecaa668
Tamila <itstamila@gmail.com>
2018-12-22 17:50:06 -0500

    Add files via upload
    NYC google map added

---
+++ src/Map.js
@@ -0,0 +1,68 @@
+
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+const MapComponent = withScriptjs(
+  withGoogleMap(props => (
+    <GoogleMap
+      defaultZoom={13}
+      defaultCenter={{
+        lat: 40.712776,
+        lng: -74.005974
+      }}
+    >
+      {props.markers &&
+        props.markers
+          .filter(marker => marker.isVisible)
+          .map((marker, idx, arr) => {
+            return (
+              <Marker
+                tabIndex="1"
+                position={{
+                  lat: marker.lat,
+                  lng: marker.lng
+                }}
+                onClick={() => props.handleMarkerClick(marker)}
+                title={marker.name}
+                key={idx}
+                animation={
+                  marker.isOpen === true
+                    ? window.google.maps.Animation.BOUNCE
+                    : window.google.maps.Animation.DROP
+                }
+              >
+                {marker.isOpen && (
+                  <InfoWindow>
+                    <div aria-label="Location Information">
+                      <h1>{marker.name}</h1>
+                      <h2>{marker.address}</h2>
+                    </div>
+                  </InfoWindow>
+                )}
+              </Marker>
+            );
+          })}
+    </GoogleMap>
+  ))
+);
+
+export default class Map extends Component {
+  render() {
+    return (
+      <MapComponent
+        {...this.props}
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBb7yKTFQ1cUJKolFJEDNurrQqJkDJb83w"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: ` 1000px`, width: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+}
